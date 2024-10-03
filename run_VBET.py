678fcef753a272f302a4fd457d3f95b37555f043
seanmstout <41929139+seanmstout@users.noreply.github.com>
2024-10-03 21:10:45 +0000

    10/03/2024 1411

--- run_VBET.py
+++ run_VBET.py
@@ -4,10 +4,10 @@ import classVBET
 class RunVBET:
     def __init__(self):
         self.params = {
-            'network': '/path/to/stream/network.shp',
-            'dem': '/path/to/dem.tif',
-            'out': '/path/to/vbet/output.shp',
-            'scratch': 'path/to/scratch/workspace',
+            'network': '/workspaces/VBET-2/VBET_Inputs/NHDFlowline_filtere_Dissolve.shp',
+            'dem': '/workspaces/VBET-2/VBET_Inputs/VBET_DEM_ProjectRast_1.tif',
+            'out': '/workspaces/VBET-2/Output',
+            'scratch': '/workspaces/VBET-2/VBET_scratch',
             'lg_da': 250,
             'med_da': 25,
             'lg_slope': 3,
@@ -17,8 +17,8 @@ class RunVBET:
             'med_buf': 200,
             'sm_buf': 80,
             'min_buf': 10,
-            'dr_area': None,
-            'da_field': 'TotDASqKm',
+            'dr_area': '/workspaces/VBET-2/VBET_Inputs/DARaster2.tif',
+            'da_field': None,
             'lg_depth': 3,
             'med_depth': 2,
             'sm_depth': 1.5
@@ -31,5 +31,5 @@ class RunVBET:
         vb.valley_bottom()
 
 
-# vbrun = RunVBET()
-# vbrun.run()
+vbrun = RunVBET()
+vbrun.run()
